87fc569f466a9164cdfd07c1436e76886d2ce40d
Eduardo-Gonz <erugonz249@gmail.com>
2021-12-06 18:23:16 -0800

    Add code for viewing employee database when adding a new employee

--- cs340_sample_nodejs_app-master/employee.js
+++ cs340_sample_nodejs_app-master/employee.js
@@ -13,13 +13,13 @@ module.exports = function(){
         });
     }
 
-    function getPeople(res, mysql, context, complete){
-        mysql.pool.query("SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id", function(error, results, fields){
+    function getEmployees(res, mysql, context, complete){
+        mysql.pool.query("SELECT Fname, Lname, Salary, Dno FROM EMPLOYEE", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.people = results;
+            context.employee = results;
             complete();
         });
     }
@@ -67,14 +67,14 @@ module.exports = function(){
         });
     }
 
-    /*Display all people. Requires web based javascript to delete users with AJAX*/
+    /*Display all employees. Requires web based javascript to delete users with AJAX*/
 
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
         context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
-        getPeople(res, mysql, context, complete);
+        getEmployees(res, mysql, context, complete);
         getPlanets(res, mysql, context, complete);
         function complete(){
             callbackCount++;
@@ -85,7 +85,7 @@ module.exports = function(){
         }
     });
 
-    /*Display all people from a given homeworld. Requires web based javascript to delete users with AJAX*/
+    /*Display all employees for a given project. Requires web based javascript to delete users with AJAX*/
     router.get('/filter/:homeworld', function(req, res){
         var callbackCount = 0;
         var context = {};
@@ -102,7 +102,7 @@ module.exports = function(){
         }
     });
 
-    /*Display all people whose name starts with a given string. Requires web based javascript to delete users with AJAX */
+    /*Display all employees whose name starts with a given string. Requires web based javascript to delete users with AJAX */
     router.get('/search/:s', function(req, res){
         var callbackCount = 0;
         var context = {};
@@ -117,81 +117,6 @@ module.exports = function(){
             }
         }
     });
-
-    /* Display one person for the specific purpose of updating people */
-
-    router.get('/:id', function(req, res){
-        callbackCount = 0;
-        var context = {};
-        context.jsscripts = ["selectedplanet.js", "updateperson.js"];
-        var mysql = req.app.get('mysql');
-        getPerson(res, mysql, context, req.params.id, complete);
-        getPlanets(res, mysql, context, complete);
-        function complete(){
-            callbackCount++;
-            if(callbackCount >= 2){
-                res.render('update-person', context);
-            }
-
-        }
-    });
-
-    /* Adds a person, redirects to the people page after adding */
-
-    router.post('/', function(req, res){
-        console.log(req.body.homeworld)
-        console.log(req.body)
-        var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO bsg_people (fname, lname, homeworld, age) VALUES (?,?,?,?)";
-        var inserts = [req.body.fname, req.body.lname, req.body.homeworld, req.body.age];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }else{
-                res.redirect('/people');
-            }
-        });
-    });
-
-    /* The URI that update data is sent to in order to update a person */
-
-    router.put('/:id', function(req, res){
-        var mysql = req.app.get('mysql');
-        console.log(req.body)
-        console.log(req.params.id)
-        var sql = "UPDATE bsg_people SET fname=?, lname=?, homeworld=?, age=? WHERE character_id=?";
-        var inserts = [req.body.fname, req.body.lname, req.body.homeworld, req.body.age, req.params.id];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
-                console.log(error)
-                res.write(JSON.stringify(error));
-                res.end();
-            }else{
-                res.status(200);
-                res.end();
-            }
-        });
-    });
-
-    /* Route to delete a person, simply returns a 202 upon success. Ajax will handle this. */
-
-    router.delete('/:id', function(req, res){
-        var mysql = req.app.get('mysql');
-        var sql = "DELETE FROM bsg_people WHERE character_id = ?";
-        var inserts = [req.params.id];
-        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
-            if(error){
-                console.log(error)
-                res.write(JSON.stringify(error));
-                res.status(400);
-                res.end();
-            }else{
-                res.status(202).end();
-            }
-        })
-    })
 
     return router;
 }();
