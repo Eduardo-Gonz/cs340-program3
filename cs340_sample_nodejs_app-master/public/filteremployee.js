27ab3482c5e466c62b079b06e0fb1022d48adeb4
Eduardo-Gonz <erugonz249@gmail.com>
2021-12-06 20:01:03 -0800

    fix query mistake

--- cs340_sample_nodejs_app-master/public/filteremployee.js
+++ cs340_sample_nodejs_app-master/public/filteremployee.js
@@ -2,5 +2,5 @@ function filterEmployeeByProject() {
     //get the id of the selected project from the filter dropdown
     var project_id = document.getElementById('project_filter').value
     //construct the URL and redirect to it
-    window.location = '/employee/filter/' + parseInt(homeworld_id)
+    window.location = '/employee/filter/' + parseInt(project_id)
 }
