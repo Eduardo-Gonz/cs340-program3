607855260cbfe8acca701071bb550a2d21f6bf8a
Eduardo-Gonz <erugonz249@gmail.com>
2021-12-06 17:24:33 -0800

    add user info to dbcon.js

--- cs340_sample_nodejs_app-master/dbcon.js
+++ cs340_sample_nodejs_app-master/dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
+  user            : 'cs340_gonzedua',
+  password        : 'Santos10',
+  database        : 'cs340_gonzedua'
 });
 module.exports.pool = pool;
